e7ca405767c060854413e745e6d5d0c3290d2975
vilksp <v.kasperavicius1@gmail.com>
2020-06-29 12:59:10 +0300

    initial commit

---
+++ src/main/java/ksp/vilius/home/Vilius/Kasperavicius/Java/Home/Task/ViliusKasperaviciusJavaHomeTaskApplication.java
@@ -0,0 +1,13 @@
+package ksp.vilius.home.Vilius.Kasperavicius.Java.Home.Task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViliusKasperaviciusJavaHomeTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViliusKasperaviciusJavaHomeTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ksp/vilius/home/Vilius/Kasperavicius/Java/Home/Task/ViliusKasperaviciusJavaHomeTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package ksp.vilius.home.Vilius.Kasperavicius.Java.Home.Task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViliusKasperaviciusJavaHomeTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
